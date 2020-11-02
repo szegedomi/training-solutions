6bd818753d767a3a9bfb8a58ea981d6025f9427e
szegedomi <31705962+szegedomi@users.noreply.github.com>
2020-11-02 22:07:52 +0100

    javaapi solution

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,12 @@
+package javaapi;
+
+public class Upper {
+
+    public static void main(String[] args) {
+
+        String string = "Hello World!";
+
+        System.out.println(string);
+        System.out.println(string.toUpperCase());
+    }
+}
