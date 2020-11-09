818b8154ead57c34dd0ea8fc8177435a5111b422
szegedomi <31705962+szegedomi@users.noreply.github.com>
2020-11-09 15:49:46 +0100

    books solution

---
+++ src/main/java/lists/Books.java
@@ -0,0 +1,41 @@
+package lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Books {
+
+    private List<String> books = new ArrayList<>();
+
+    public void add(String book){
+        this.books.add(book);
+    }
+
+    public List<String> getTitles(){
+        return books;
+    }
+
+    public List<String> findAllByPrefix(String prefix){
+        List<String> result = new ArrayList<>();
+        for(String book : this.books){
+            if(book.contains(prefix)) {
+                result.add(book);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args){
+
+        Books books = new Books();
+
+        books.add("Lord of the Rings");
+        books.add("The Hobbit");
+        books.add("The prisoner of Ascaban");
+
+        System.out.println(books.getTitles().toString());
+        System.out.println(books.findAllByPrefix("of").toString());
+
+    }
+
+}
