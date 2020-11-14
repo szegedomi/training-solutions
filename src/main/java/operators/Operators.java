e13218326067b0996e5a50b7cf2202b29b945692
szegedomi <31705962+szegedomi@users.noreply.github.com>
2020-11-14 15:43:47 +0100

    operators solution

---
+++ src/main/java/operators/Operators.java
@@ -0,0 +1,13 @@
+package operators;
+
+public class Operators {
+
+    public boolean isEven(int n){
+        return n%2 == 0;
+    }
+
+    public int multiplyByPowerOfTwo(int a, int pow){
+        return a << pow;
+    }
+
+}
---
+++ src/main/java/operators/OperatorsMain.java
@@ -0,0 +1,15 @@
+package operators;
+
+
+public class OperatorsMain {
+
+    public static void main(String[] args){
+
+        Operators operator = new Operators();
+        System.out.println(operator.multiplyByPowerOfTwo(3, 4));
+        System.out.println(operator.isEven(operator.multiplyByPowerOfTwo(3, 4)));
+        System.out.println(0333);
+
+    }
+
+}
