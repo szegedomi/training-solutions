1a932bf7bc8ad7d41690025af7f12c338a8f861d
szegedomi <31705962+szegedomi@users.noreply.github.com>
2020-11-14 16:54:44 +0100

    intandfloat solutions

---
+++ src/main/java/intandfloat/Circle.java
@@ -0,0 +1,35 @@
+package intandfloat;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Circle {
+
+    private int diameter;
+    private static double PI = 3.14;
+
+    public Circle(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public double perimeter(int diameter){
+        return diameter/2.0*2*PI;
+    }
+    public double area(int diameter){
+        return diameter/2.0*diameter/2*PI;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Circle circle = new Circle(20);
+
+        System.out.println("Please provide the diameter of the circle:");
+        int diameter = scanner.nextInt();
+
+        System.out.println("Area is: " + circle.area(diameter));
+        System.out.println("Perimeter is: " + circle.perimeter(diameter));
+
+    }
+}
---
+++ src/main/java/intandfloat/MathOperations.java
@@ -0,0 +1,20 @@
+package intandfloat;
+
+import java.util.Scanner;
+
+public class MathOperations {
+
+    public static void main(String[] args) {
+
+        System.out.println("Please solve the following problem!");
+        System.out.println("((20/3 + 3) * 5) - 1");
+
+        Scanner scanner = new Scanner(System.in);
+        double solution = scanner.nextDouble();
+
+        System.out.println(Math.abs((((20/3.0+3)*5)-1) - solution) < 0.0001);
+
+
+    }
+
+}
