4598ab52bf72c3445c8c95b9de400088137d0e20
szegedomi <31705962+szegedomi@users.noreply.github.com>
2020-11-20 20:17:35 +0100

    Lybrary solution

---
+++ src/main/java/filescanner/Library.java
@@ -0,0 +1,30 @@
+package filescanner;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Library {
+
+    private List<Book> books = new ArrayList<>();
+
+    public void loadFromFile(){
+       try (Scanner scanner = new Scanner(Library.class.getResourceAsStream("/books.csv"))){
+           scanner.useDelimiter(";|(\r\n)|\n");
+           while(scanner.hasNextLine()) {
+               String regNum = scanner.next();
+               String author = scanner.next();
+               String title = scanner.next();
+               int yearOfPublish = scanner.nextInt();
+               this.books.add(new Book(regNum, author, title, yearOfPublish));
+           }
+       }
+    }
+
+    public List<Book> getBooks(){
+        return this.books;
+    }
+
+}
