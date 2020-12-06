75cf44dd37e0dcbeacad9abc1b36f5900373a42d
szegedomi <31705962+szegedomi@users.noreply.github.com>
2020-12-06 20:20:52 +0100

    JavaBeans gyakorlati feladat megoldás

---
+++ src/main/java/javabeans/Human.java
@@ -0,0 +1,38 @@
+package javabeans;
+
+public class Human {
+
+    private String name;
+    private double weight;
+    private int iq;
+
+    public Human(String name, double weight, int iq) {
+        this.name = name;
+        this.weight = weight;
+        this.iq = iq;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getIq() {
+        return iq;
+    }
+
+    public void setIq(int iq) {
+        this.iq = iq;
+    }
+}
