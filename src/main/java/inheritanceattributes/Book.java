e35d9ff90986e5f82ee5616bed1f873107da0cde
szegedomi <31705962+szegedomi@users.noreply.github.com>
2020-12-08 21:40:31 +0100

    inheritanceattributes/Book solution

---
+++ src/main/java/inheritanceattributes/Book.java
@@ -0,0 +1,21 @@
+package inheritanceattributes;
+
+public class Book {
+
+    private String title;
+    protected int price;
+
+    public Book(String title, int price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int purchase(int pieces){
+        return price*pieces;
+    }
+
+}
---
+++ src/main/java/inheritanceattributes/ShippedBook.java
@@ -0,0 +1,20 @@
+package inheritanceattributes;
+
+public class ShippedBook extends Book{
+
+    private int shippingCost;
+
+    public ShippedBook(String title, int price, int shippingCost) {
+        super(title, price);
+        this.shippingCost = shippingCost;
+    }
+
+    public int order(int pieces){
+        return price * pieces + shippingCost;
+    }
+
+    public String toString() {
+        return getTitle() + ":" + price + " - " + shippingCost;
+    }
+
+}
