a7a66875c8140c2f20faf549f0baa095c1c7bd08
szegedomi <31705962+szegedomi@users.noreply.github.com>
2021-01-26 23:41:33 +0100

    week02 day03 solution Product

---
+++ src/main/java/week02/week02d03/Product.java
@@ -0,0 +1,25 @@
+package week02.week02d03;
+
+public class Product {
+
+    private String name;
+    private String code;
+
+    public Product(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public boolean areTheyEqual(Product product) {
+        return this.name.equals(product.getName());
+    }
+
+}
